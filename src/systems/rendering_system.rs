4a4ff2d62c0da363fd2f664b44ac89a2289b2183
Brian Chen <brianchen8990@gmail.com>
2022-06-24 21:52:02 +0200

    Add batch rendering

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 use crate::{
     components::{Position, Renderable, RenderableKind},
@@ -7,10 +7,11 @@ use crate::{
 };
 
 use ggez::{
-    graphics::{self, Color, DrawParam, Image},
-    Context,
+    graphics::{self, spritebatch::SpriteBatch, Color, DrawParam, Image},
+    timer, Context,
 };
 use glam::Vec2;
+use itertools::Itertools;
 use specs::{join::Join, Read, ReadStorage, System};
 
 pub struct RenderingSystem<'a> {
@@ -34,7 +35,7 @@ impl RenderingSystem<'_> {
         .expect("expected drawing queued text");
     }
 
-    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> Image {
+    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> String {
         let path_index = match renderable.kind() {
             RenderableKind::Static => {
                 // We only have one image, so we just return that
@@ -50,9 +51,7 @@ impl RenderingSystem<'_> {
             }
         };
 
-        let image_path = renderable.path(path_index);
-
-        Image::new(self.context, image_path).expect("expected image")
+        renderable.path(path_index)
     }
 }
 
@@ -74,25 +73,52 @@ impl<'a> System<'a> for RenderingSystem<'a> {
 
         // Get all the renderables with their positions and sort by the position z
         // This will allow us to have entities layered visually.
-        let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
-        rendering_data.sort_by_key(|&k| k.0.z);
+        let rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
+        let mut rendering_batches: HashMap<u8, HashMap<String, Vec<DrawParam>>> = HashMap::new();
 
         // Iterate through all pairs of positions & renderables, load the image
         // and draw it at the specified position.
         for (position, renderable) in rendering_data.iter() {
             // Load the image
-            let image = self.get_image(renderable, time.delta);
+            let image_path = self.get_image(renderable, time.delta);
             let x = position.x as f32 * TILE_WIDTH;
             let y = position.y as f32 * TILE_WIDTH;
+            let z = position.z;
 
             // draw
-            let draw_params = DrawParam::new().dest(Vec2::new(x, y));
-            graphics::draw(self.context, &image, draw_params).expect("expected render");
+            let draw_param = DrawParam::new().dest(Vec2::new(x, y));
+            rendering_batches
+                .entry(z)
+                .or_default()
+                .entry(image_path)
+                .or_default()
+                .push(draw_param);
+        }
+
+        // Iterate spritebatches ordered by z and actually render each of them
+        for (_z, group) in rendering_batches
+            .iter()
+            .sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
+        {
+            for (image_path, draw_params) in group {
+                let image = Image::new(self.context, image_path).expect("expected image");
+                let mut sprite_batch = SpriteBatch::new(image);
+
+                for draw_param in draw_params.iter() {
+                    sprite_batch.add(*draw_param);
+                }
+
+                graphics::draw(self.context, &sprite_batch, graphics::DrawParam::new())
+                    .expect("expected render");
+            }
         }
 
         // Render any text
         self.draw_text(&gameplay.state.to_string(), 525.0, 80.0);
         self.draw_text(&gameplay.moves_count.to_string(), 525.0, 100.0);
+
+        let fps = format!("FPS: {:.0}", timer::fps(self.context));
+        self.draw_text(&fps, 525.0, 120.0);
 
         // Finally, present the context, this will actually display everything
         // on the screen.
